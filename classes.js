c3e44042f04ce543ab19ef77e6dc501a64ff36be
jinjanow <arashiyama1120@gmail.com>
2022-03-05 11:16:50 +0900

    Initial commit

---
+++ classes.js
@@ -0,0 +1,456 @@
+export const on = (n, fn) => document.addEventListener(n, fn);
+export const emit = (e) =>
+  document.dispatchEvent(e);
+export const deleteLis = (n, fn) => document.removeEventListener(n, fn);
+export const atan = (x, y) => {
+  if (x === 0 && y === 0) {
+    return 0;
+  }
+  if (x === 0) {
+    if (y > 0) {
+      return Math.PI / 2;
+    }
+    return (Math.PI * 3) / 2;
+  }
+  if (x < 0) {
+    return Math.atan(y / x) + Math.PI;
+  }
+  let t = Math.atan(y / x);
+  if (t > 0) return t;
+  if (t == 0) return 0;
+  return Math.PI * 2 + t;
+};
+let views = {
+  user: {
+    size: 16,
+    colors: ['blue', 'erase', 'red'],
+    display: [
+      [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ],
+  },
+  ball: {
+    size: 16,
+    colors: ['white', 'erase'],
+    display: [
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ],
+  },
+};
+export class User {
+  constructor(canvas) {
+    this.x = window.innerWidth / 2;
+    this.y = (window.innerHeight * 1.4) / 2;
+    this.speed = 5;
+    this.view = views.user;
+    this.canvas = canvas;
+    this.dotCanvas = new DotCanvas(canvas, 1);
+    this.key = [];
+    this.angle = 1;
+    on('drow', this.drow.bind(this));
+    on('touchuser', this.touchUser.bind(this));
+    window.addEventListener('keydown', (e) => {
+      if (this.key.includes(e.key)) return;
+      this.key.push(e.key);
+      this.key = this.key.filter(
+        (k) => k != { d: 'a', s: 'w', a: 'd', w: 's' }[e.key]
+      );
+    });
+    window.addEventListener('keyup', (e) => {
+      this.key = this.key.filter((k) => k != e.key);
+    });
+    this.intervalHandle = setInterval(() => {
+      if (this.key.length === 0) return;
+      for (const k of this.key) {
+        switch (k) {
+          case 'ArrowRight':
+            this.x += this.speed;
+            break;
+          case 'd':
+            this.x += this.speed;
+            break;
+          case 'ArrowLeft':
+            this.x -= this.speed;
+            break;
+          case 'a':
+            this.x -= this.speed;
+            break;
+          case 'ArrowUp':
+            this.y -= this.speed;
+            break;
+          case 'w':
+            this.y += this.speed;
+            break;
+          case 'ArrowDown':
+            this.y += this.speed;
+            break;
+          case 's':
+            this.y -= this.speed;
+            break;
+        }
+      }
+    }, 1000 / 30);
+  }
+  drow() {
+    this.dotCanvas.import(
+      this.view,
+      this.x - 5,
+      (this.y + 5) * -1 + innerHeight,
+      5
+    );
+  }
+  touchUser() {
+    //console.log('touch');
+  }
+}
+export class Ball {
+  constructor(canvas, x = 200, y = 300) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.dotCanvas = new DotCanvas(canvas);
+    this.angle = 0;
+    this.x = x;
+    this.y = y;
+    this.view = views.ball;
+    this.speed = 3;
+    this.delete = false;
+    let d = this.drow.bind(this);
+    this.phase = 0;
+    this.out = false;
+    this.outing = false;
+    this.outTime = 0;
+    on('drow', d);
+    on('move', this.move.bind(this));
+    setInterval(() => {
+      if (this.delete) return;
+      this.onWall();
+      this.isTouchUser();
+      this.out =
+        this.x < 0 ||
+        this.x > window.innerWidth ||
+        this.y < 0 ||
+        this.y > window.innerHeight;
+    }, 1000 / 30);
+  }
+  drow() {
+    if (this.delete) return;
+    this.dotCanvas.import(this.view, this.x, this.y * -1 + innerHeight, 5);
+  }
+  move() {
+    if (this.delete) return;
+    this.x += Math.cos(this.angle) * this.speed;
+    this.y += Math.sin(this.angle) * this.speed;
+  }
+  honet(x, y) {
+    this.angle = atan(x - this.x, y - this.y);
+    return this;
+  }
+  onWall() {
+    if (
+      this.outing &&
+      0 < this.x &&
+      this.x < innerWidth &&
+      0 < this.y &&
+      this.y < innerHeight
+    ) {
+      this.outing = false;
+      return;
+    }
+    if (this.outing || !this.out) return;
+    this.outTime++;
+    if (this.x > innerWidth) {
+      this.angle =
+        Math.atan((Math.sin(this.angle) / Math.cos(this.angle)) * -1) + Math.PI;
+    } else if (Math.PI / 2 <= this.angle && this.angle <= (Math.PI / 2) * 3) {
+      if (this.x <= 0) {
+        this.angle = Math.atan(
+          (Math.sin(this.angle) / Math.cos(this.angle)) * -1
+        );
+      } else
+        this.angle =
+          Math.atan((Math.sin(this.angle) / Math.cos(this.angle)) * -1) +
+          Math.PI;
+    } else
+      this.angle = Math.atan(
+        (Math.sin(this.angle) / Math.cos(this.angle)) * -1
+      );
+    this.outing = true;
+  }
+}
+export class DotCanvas {
+  /**
+   * @param {HTMLCanvasElement} canvas ドット絵を表示・作成するcanvasを指定
+   * @param {number} size ドット絵の解像度を指定します
+   */
+  constructor(canvas, size) {
+    this.backgroundColor = 'black';
+    this.canvas = canvas;
+    this.size = size;
+    this.display = Array(size)
+      .fill(0)
+      .map(() => Array(size).fill(0));
+    this.pixcel = this.canvas.width / this.size;
+    this.colors = [
+      'erase',
+      'white',
+      'grey',
+      'black',
+      'red',
+      'orange',
+      'yellow',
+      'lime',
+      'green',
+      'darkgreen',
+      'blue',
+      'midnightblue',
+      'purple',
+    ];
+    this.color = 5;
+    this.click = false;
+    this.grid = 0;
+    this.ctx = this.canvas.getContext('2d');
+    this.canvasMousemoveEvent = (e) => {
+      this.mouseX = Math.floor(e.offsetX / this.pixcel);
+      this.mouseY = Math.floor(e.offsetY / this.pixcel);
+      if (this.click) {
+        this.display[this.mouseX][this.mouseY] = this.color;
+        this.ctx.fillStyle = this.backgroundColor;
+        this.ctx.fillRect(0, 0, this.canvas.height, this.canvas.width);
+        this.drowAllPixcel();
+      }
+    };
+    this.canvasTouchmoveEvent = (e) => {
+      let tgt = e.touches[0].target.getBoundingClientRect();
+      this.mouseX = Math.floor((e.touches[0].pageX - tgt.top) / this.pixcel);
+      this.mouseY = Math.floor((e.touches[0].pageY - tgt.left) / this.pixcel);
+      if (
+        this.mouseX < 0 ||
+        this.mouseY < 0 ||
+        this.size - 1 < this.mouseX ||
+        this.size - 1 < this.mouseY
+      )
+        return;
+      this.display[this.mouseX][this.mouseY] = this.color;
+      this.ctx.fillStyle = this.backgroundColor;
+      this.ctx.fillRect(0, 0, this.canvas.height, this.canvas.width);
+      this.drowAllPixcel();
+    };
+
+    this.canvasClickEvent = () => {
+      this.display[this.mouseX][this.mouseY] = this.color;
+      this.ctx.fillStyle = this.backgroundColor;
+      this.ctx.fillRect(0, 0, this.canvas.height, this.canvas.width);
+      this.drowAllPixcel();
+    };
+    this.canvasMousedownEvent = () => {
+      this.click = true;
+    };
+    this.canvasMouseupEvent = () => {
+      this.click = false;
+    };
+    this.canvas.addEventListener('mousemove', this.canvasMousemoveEvent);
+    this.canvas.addEventListener('touchmove', this.canvasTouchmoveEvent);
+    this.canvas.addEventListener('click', this.canvasClickEvent);
+    this.canvas.addEventListener('mousedown', this.canvasMousedownEvent);
+    this.canvas.addEventListener('mouseup', this.canvasMouseupEvent);
+    this.toReadOnly();
+  }
+  set setBackGroundColor(num) {
+    this.backgroundColor = `rgb(${num},${num},${num})`;
+    this.ctx.fillStyle = this.backgroundColor;
+    this.ctx.fillRect(0, 0, this.canvas.height, this.canvas.width);
+    this.drowAllPixcel();
+  }
+  gridPlus() {
+    this.grid++;
+    if (this.grid === 3) {
+      this.grid = 0;
+    }
+    switch (this.grid) {
+      case 0:
+        this.drowGrid();
+        break;
+      case 1:
+        this.disableGild();
+        this.ctx.strokeStyle = 'black';
+        this.ctx.strokeRect(0, 0, this.canvas.height, this.canvas.width);
+        break;
+      case 2:
+        this.disableGild();
+    }
+    this.ctx.fillStyle = this.backgroundColor;
+    this.ctx.fillRect(0, 0, this.canvas.height, this.canvas.width);
+    this.drowAllPixcel();
+  }
+  toReadOnly() {
+    this.canvas.removeEventListener('mousemove', this.canvasMousemoveEvent);
+    this.canvas.removeEventListener('touchmove', this.canvasTouchmoveEvent);
+    this.canvas.removeEventListener('click', this.canvasClickEvent);
+    this.canvas.removeEventListener('mousedown', this.canvasMousedownEvent);
+    this.canvas.removeEventListener('mouseup', this.canvasMouseupEvent);
+    this.grid = 2;
+    this.disableGild();
+    return this;
+  }
+  drowGrid() {
+    let cs = this.canvas.width;
+    let pixcel = cs / this.size;
+    this.ctx.strokeStyle = 'black';
+    for (let i = 0; i <= this.size; i++) {
+      let y = pixcel * i;
+      this.ctx.moveTo(0, y);
+      this.ctx.lineTo(cs, y);
+    }
+    for (let i = 0; i <= this.size; i++) {
+      let x = pixcel * i;
+      this.ctx.moveTo(x, 0);
+      this.ctx.lineTo(x, cs);
+    }
+    this.ctx.stroke();
+    return this;
+  }
+  disableGild() {
+    this.drowAllPixcel();
+    return this;
+  }
+  drowAllPixcel(offsetX = 0, offsetY = 0) {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        this.fillDot(i, j, this.display[i][j], offsetX, offsetY);
+      }
+    }
+    this.ctx.fillStyle = this.backgroundColor;
+    this.ctx.strokeStyle = 'black';
+    if (this.grid === 0) {
+      this.drowGrid();
+    } else if (this.grid === 1) {
+      this.ctx.strokeRect(0, 0, this.canvas.height, this.canvas.width);
+    }
+  }
+  fillDot(x, y, color, offsetX, offsetY) {
+    if (this.colors[color] === 'erase') return;
+    this.ctx.fillStyle = this.colors[color];
+    this.ctx.strokeStyle = this.colors[color];
+    this.ctx.fillRect(
+      x * this.pixcel + offsetX,
+      y * this.pixcel + offsetY,
+      this.pixcel,
+      this.pixcel
+    );
+    this.ctx.strokeRect(
+      x * this.pixcel + offsetX,
+      y * this.pixcel + offsetY,
+      this.pixcel,
+      this.pixcel
+    );
+  }
+  /**
+   * @param {object} option
+   * compression(boolean):圧縮して出力するか
+   * json(boolean):jsonで出力するか
+   */
+  export(option) {
+    if (option?.compression) {
+      let usedcolor = [];
+      for (let color of this.display.flat()) {
+        if (!usedcolor.includes(color)) {
+          usedcolor.push(color);
+        }
+      }
+      let colors = usedcolor.map((v) => this.colors[v]);
+      let links = colors.map((v) => this.colors.indexOf(v));
+      links.forEach((v, i, a) => {
+        a[v] = i;
+      });
+      let dis = this.display.map((line) => {
+        return line.map((v) => links[v]);
+      });
+      let obj = {
+        size: this.size,
+        colors: colors,
+        display: dis,
+      };
+      if (option?.json) {
+        return JSON.stringify(obj);
+      }
+      return obj;
+    }
+    if (option?.json) {
+      return JSON.stringify({
+        size: this.size,
+        colors: this.colors,
+        display: this.display,
+      });
+    }
+    return {
+      size: this.size,
+      colors: this.colors,
+      display: this.display,
+    };
+  }
+  /**
+   * @param {string} data dotCanvasクラスでexportされたJSON文字列あるいはオブジェクト
+   * @param {number} offsetX x軸方向の描画の起点の座標
+   * @param {number} offsetY y軸方向の描画の起点の座標
+   * @param {number} pixcelSize 一つのピクセルの大きさ
+   */
+  import(data, offsetX = 0, offsetY = 0, pixcelSize) {
+    let d = typeof data === 'string' ? JSON.parse(data) : data;
+    if (
+      d.display
+        .map((a) => a.length)
+        .map((n) => n === d.size)
+        .includes(false) ||
+      d.display.length !== d.size
+    ) {
+      console.log('missmatch');
+      return this;
+    }
+    this.colors = d.colors;
+    this.display = d.display;
+    this.size = d.size;
+    this.pixcel =
+      pixcelSize === undefined ? this.canvas.width / this.size : pixcelSize;
+    this.drowAllPixcel(offsetX, offsetY);
+    return this;
+  }
+  getAllImgData() {
+    let s = this.size;
+    let oldData = this.export();
+    let elm = buildElement('canvas', {
+      height: s,
+      width: s,
+    });
+    let d = new dotCanvas(elm).import(oldData, 1).toReadOnly();
+    return elm.getContext('2d').getImageData(0, 0, s, s);
+  }
+}
---
+++ script.js
@@ -0,0 +1,192 @@
+const canvas = document.getElementById('can');
+canvas.height = innerHeight;
+canvas.width = innerWidth;
+const ctx = canvas.getContext('2d');
+import { User, DotCanvas, on, emit, Ball, atan } from './classes.js';
+Number.prototype.in = function (min, max) {
+  return min <= this && this <= max;
+};
+let events = {
+  drow: new Event('drow'),
+  move: new Event('move'),
+  touchuser: new Event('touchuser'),
+};
+on('drow', () => {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, innerWidth, innerHeight);
+});
+const user = new User(canvas);
+user.drow();
+class Hound extends Ball {
+  constructor(c, x, y, t) {
+    super(c, x, y);
+    this.type = t;
+    this.view =
+      '{"size":16,"colors":["erase","red","grey","black","red","orange","yellow","lime","green","darkgreen","blue","midnightblue","purple"],"display":[[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]}';
+  }
+  onWall() {
+    if (this.phase == 1) return;
+    super.onWall();
+  }
+  isTouchUser() {
+    if (this.x.in(user.x - 3, user.x + 20) && this.y.in(user.y - 20, user.y)) {
+      emit(events.touchuser);
+      this.delete = true;
+    }
+  }
+  honet(x, y) {
+    if (this.phase === 0 && this.type === 0) {
+      super.honet(
+        innerWidth / 2 +
+          (Math.random() - 0.5) * 10 +
+          150 * Math.sign(this.x - x),
+        innerHeight
+      );
+      this.speed = 5;
+      this.phase = 1;
+      setTimeout(() => {
+        this.phase = 2;
+      }, 2500);
+      return this;
+    }
+
+    if (this.type === 1 && this.phase === 0) {
+      super.honet(x, y);
+      this.phase = 2;
+      setTimeout(() => {
+        this.phase = 3;
+      }, 5000);
+    }
+    if (this.phase === 1) {
+      setTimeout(() => {
+        this.phase = 3;
+      }, 10000);
+    }
+    if (this.phase == 1 || this.phase == 3) return;
+    if (this.speed > 1) this.speed -= 0.05;
+    let moveAngle = atan(x - this.x, y - this.y) - this.angle;
+    //if (moveAngle.in(-0.1, 0.1)) this.f = 2;
+    let n = 0.05;
+    if (Math.abs(moveAngle) > n) {
+      if (moveAngle.in(0, Math.PI)) {
+        this.angle += n;
+      } else {
+        this.angle -= n;
+      }
+      if (this.angle >= Math.PI * 2) this.angle -= Math.PI * 2;
+      if (this.angle <= 0) this.angle += Math.PI * 2;
+    } else super.honet(x, y);
+    return this;
+  }
+}
+class Asteroid extends Ball {
+  isTouchUser() {
+    if (this.x.in(user.x - 3, user.x + 20) && this.y.in(user.y - 20, user.y)) {
+      emit(events.touchuser);
+      this.delete = true;
+    }
+  }
+  honet(x, y) {
+    if (this.phase === 0) {
+      this.speed = 5;
+      //super.honet(innerWidth * Math.random(), y);
+      super.honet(x, y);
+      this.phase = 1;
+    } else {
+      super.honet(x, y);
+    }
+    return this;
+  }
+  onWall() {
+    if (this.out) this.delete = true;
+  }
+}
+let balls = [];
+let fence = Array(63)
+  .fill(0)
+  .map((_, i) => {
+    let ins = new Asteroid(
+      canvas,
+      150 * Math.cos(i / 10) + innerWidth / 2,
+      150 * Math.sin(i / 10) + innerHeight / 2
+    );
+    ins.speed = 0;
+  });
+let a = Math.PI / 2 - 0.3;
+setInterval(() => {
+  a += Math.PI / 3 - 0.01;
+  let ins = new Asteroid(canvas, innerWidth / 2, innerHeight / 2);
+  ins.angle = a;
+  balls.push(ins);
+  emit(events.move);
+  emit(events.drow);
+  balls = balls.filter((ball) => !ball.delete);
+}, 1000 / 30);
+
+{
+  let b = 0;
+  let c = 0;
+  let d = 0;
+  let b2 = 0;
+  let c2 = innerWidth;
+  let d2 = 0;
+  setInterval(() => {
+    if (d % 2 === 0) {
+      c += 10;
+    } else {
+      c -= 10;
+    }
+    if (c > innerWidth || c < 0) d++;
+    if (++b % 5 === 0)
+      balls.push(new Asteroid(canvas, innerWidth, 0).honet(c, innerHeight));
+    if (d2 % 2 === 0) {
+      c2 += 10;
+    } else {
+      c2 -= 10;
+    }
+    if (c2 > innerWidth || c2 < 0) d2++;
+    if (++b2 % 5 === 0)
+      balls.push(new Asteroid(canvas, 0, 0).honet(c2, innerHeight));
+    emit(events.drow);
+    emit(events.move);
+    for (let i = 0; i < balls.length; i++) {
+      let ball = balls[i];
+      if (ball.outTime === 1) ball.delete = true;
+      if (ball.delete) {
+        balls[i] = undefined;
+        balls = balls.filter((v) => v != undefined);
+      }
+    }
+  }, 1000 / 20);
+}
+/*
+setInterval(() => {
+  if (++a % 2 === 0 && balls.length < 100){
+    if(Math.random()<0.5){
+      balls.push(
+        new Haund(
+          canvas,
+          Math.random() < 0.5 ? 10 : innerWidth - 10,
+          0,
+          //Math.round(Math.random())
+          0
+        )
+      );
+    }else{
+      balls.push(new Asteroid(canvas,innerWidth/2,1))
+    }
+  }
+    
+  a = a % 2;
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.honet(user.x, user.y);
+    ball.move();
+    if (ball.delete) {
+      balls[i] = undefined;
+      balls = balls.filter((v) => v != undefined);
+    }
+  }
+  emit('drow');
+}, 1000 / 30);
+*/
